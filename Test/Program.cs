cdb53bf991411b3a7c238734920cca16d97115b5
Michael Townley <birdie4mt@hotmail.com>
2023-10-13 11:06:31 -0400

    GitLearning

---
+++ Test/Program.cs
@@ -0,0 +1,48 @@
+﻿
+string itemName;
+
+string[] items = { "Spam", "Apples", "Onions", "Snickers Bar" };
+
+for (int i = 0; i < items.Length; i++)
+{
+    Console.WriteLine(i + 1 + ") " + items[i]);
+}
+
+string input = Console.ReadLine();
+int pick = int.Parse(input);
+Console.WriteLine(items[pick - 1]);
+
+/*char letter = 'A';
+for (int i = 0; i < items.Length; i++)
+{
+    Console.WriteLine(letter + ") " + items[i]);
+    //Chars can be treat mathematically. On the ascii chart, each has its numeric index. 
+    //By doing char++ I'm saying go to the next index in the chart 
+    //All upper and lowercase characters are all grouped together in the ASCii
+    letter++;
+}
+Dictionary<char, int> charactersToIndexes = new Dictionary<char, int>();
+letter = 'A';
+for (int i = 0; i < items.Length; i++)
+{
+    charactersToIndexes.Add(letter, i);
+    letter++;
+}*/
+
+Dictionary<char, int> charactersToIndexes = new Dictionary<char, int>();
+
+char letter = 'A';
+for (int i = 0; i < items.Length; i++)
+{
+    Console.WriteLine(letter + ") " + items[i]);
+    charactersToIndexes.Add(letter, i);
+    //Chars can be treat mathematically. On the ascii chart, each has its numeric index. 
+    //By doing char++ I'm saying go to the next index in the chart 
+    //All upper and lowercase characters are all grouped together in the ASCii
+    letter++;
+}
+
+
+char selected = char.Parse(Console.ReadLine().ToUpper());
+int index = charactersToIndexes[selected];
+Console.WriteLine(items[index]);
